81973756dc74d04e3afd3739f8d68b38ca213b67
TimurbekMansurov <mansurovt72@gmail.com>
2022-03-28 18:55:46 +0300

    сделал главную страницу(почти)

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // },
+  {
+    path: '/',
+    name: 'Main',
+    component: () => import('../views/Main.vue')
+  },
 ]
 
 const router = new VueRouter({
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,23 +1,23 @@
 <template>
-  <div>
-    <div>
-        <h2> Викторина по IT</h2>
+  <div class="main">
+    <div class="header">
+        <h1> Викторина по IT</h1>
     </div>
-    <div>
-        <h4>Начать викторину</h4> 
+    <div class="quizing">
+        <h4 class="h4">Начать викторину</h4>
+        <p class="p">Вам зададут 10 рандомно выбранных вопросов.</p> 
     </div>
-        <p>Вам зададут 10 рандомно выбранных вопросов.</p>
-    <div>
-        <h4>Викторина на определенную тему</h4>
-        <p>Выберите тему викторины, после чего все вопросы будут касательно выбранной темы.</p>
+     <div class="themes">
+        <h4 class="h4">Викторина на определенную тему</h4>
+        <p class="p">Выберите тему викторины, после чего все вопросы будут касательно выбранной темы.</p>
     </div>
-    <div>
-        <h4>История поиска</h4>
-        <p>Узнайте результат пройденных ранее викторин, дату прохождения и балл.</p>
+    <div class="history">
+        <h4 class="h4">История</h4>
+        <p class="p">Узнайте результат пройденных ранее викторин, дату прохождения и балл.</p>
     </div>
-    <div>
-        <p>Наш номер телефона: +7 968 *** ** **</p>
-        <p>Наша почта: victorina@gmail.com</p>
+    <div class="basement">
+        <p class="p">Наш номер телефона: +7 968 *** ** **</p>
+        <p class="p">Наша почта: victorina@gmail.com</p>
     </div>
   </div>
 </template>
@@ -29,34 +29,52 @@ export default {
 </script>
 
 <style>
-.h2 {
-color: rgb(10, 56, 2);
+.h1 {
+    color: rgb(80, 112, 21);
+    text-align: center;
+    font-size: 40px;
 }
 .h4 {
-color: rgb(10, 56, 2);
+    color: rgb(80, 112, 21);
+    text-align: center;
+    font-size: 32px;
 }
 .p {
-color: rgb(10, 56, 2);
+    color: rgb(80, 112, 21);
+    text-align: center;
+    font-size: 24px;
 }
 .header {
-color: rgb(17, 148, 28);
-border: 8px; 
-border-color: rgb(17, 148, 28);
+    color: rgb(80, 112, 21);
+    text-align: center;
 }
 .basement {
-color: rgb(17, 148, 28);
-
+    color: rgb(80, 112, 21);
+    border-top: 6px solid rgb(0, 0, 0);
+    text-align: center;
 }
 .quizing {
-color: rgb(17, 148, 28);
-
+    color: rgb(80, 112, 21);
+    border: 6px solid rgb(0, 0, 0);
+    text-align: center;
+    margin: 42px;
+    background: rgb(141, 195, 90);
 }
 .themes {
-color: rgb(17, 148, 28);
-
+    color: rgb(80, 112, 21);
+    border: 6px solid rgb(0, 0, 0);
+    text-align: center;
+    margin: 42px;
+    background: rgb(141, 195, 90);
 }
 .history {
-color: rgb(17, 148, 28);
-
+    color: rgb(80, 112, 21);
+    border: 6px solid rgb(0, 0, 0);
+    text-align: center;
+    margin: 42px;
+    background: rgb(141, 195, 90);
+}
+.main {
+     background: rgb(159, 180, 67);
 }
 </style>
